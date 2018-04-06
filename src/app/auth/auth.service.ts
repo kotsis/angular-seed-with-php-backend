72bd38b8a9738fd7773aacee42a20ef8d4e358ee
Konstantinos Katsamakas <kmak@it.auth.gr>
2018-04-07 02:28:45 +0300

    AuthService and guard changes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { environment } from './../../environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  authenticated: boolean;
 
+  constructor(private router: Router) {
+    this.checkIfLoggedin();
+  }
+
+  //let options = new RequestOptions({ headers: headers, withCredentials: true });
+  //this.http.post(this.connectUrl, <stringified_data> , options)
+
+  checkIfLoggedin(){
+    //ask api
+    //to-do use environment.apiUrl+'/isLoggedIn' returns yes, no
+    this.authenticated = false;
+  }
+
+  get isLoggedIn(): boolean {
+    // Check if current date is greater
+    // than expiration and user is logged in
+    //const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    //return Date.now() < expiresAt && this.authenticated;
+    return this.authenticated;
+  }
 }
