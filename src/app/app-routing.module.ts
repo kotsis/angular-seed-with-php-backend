a58553632be27fe9a4df4cf2405d205480c68e7d
Konstantinos Katsamakas <kmak@it.auth.gr>
2018-04-07 02:26:22 +0300

    Login secondary route and popup fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+// Import the AuthGuard
+import { AuthGuard } from './auth/auth.guard';
+
+import { LoginComponent }   from './login/login.component';
+import { HomeComponent }   from './home/home.component';
+import { ProtectedComponent }   from './protected/protected.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, outlet: 'popup' },
+  { path: 'home', component: HomeComponent },
+  { path: 'protected', component: ProtectedComponent,
+    // Add this to guard this route
+    canActivate: [
+      AuthGuard
+    ]
+  },
+  //{ path: 'detail/:id', component: HeroDetailComponent },
+  //{ path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
+  // Add AuthGuard to the providers array
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
 
 import * as $ from 'jquery';
 import 'bootstrap';
@@ -10,14 +11,23 @@ import 'bootstrap';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
 	//show login modal!
         $('#loginModal').modal({
             show: true
         });
+        $('#loginModal').on('hidden.bs.modal',  () => {
+            // do something...
+            this.closePopup();
+        })
         //alert('login!');
   }
 
+  closePopup() {
+    // Providing a `null` value to the named outlet
+    // clears the contents of the named outlet
+    this.router.navigate([{ outlets: { popup: null }}]);
+  }
 }
